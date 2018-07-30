6ea3c4e12bce4e882d04cec9c5ffb3804a6ae4ea
Bhushan Chirmade <bhushanchirmade@gmail.com>
2018-07-29 22:39:15 -0500

    Initial import

---
+++ src/main/java/com/bbc/sample/jenkinsxgradledemo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.bbc.sample.jenkinsxgradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
